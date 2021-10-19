0c19e64859a5e77a4f47fb53f8c7e1e23d6f53e9
Harveylwo <my751721909@gmail.com>
2021-10-20 00:54:10 +0800

    Structure Created

---
+++ src/main/java/com/putterfly/simpk/app/Main.java
@@ -0,0 +1,12 @@
+package com.putterfly.simpk.app;
+
+import javafx.application.Application;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "FUCK MAVEN")
+public class Main {
+    public static void main(String[] args) {
+        log.info("Finally Working!!!!!!!!!!!!!!");
+        Application.launch(App.class);
+    }
+}
