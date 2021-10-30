d0f0491d9bd8efa927d63c637c28f995caeaeace
P7XXDMX <my751721909@gmail.com>
2021-10-31 00:51:04 +0800

    Optimized Drawer Logics

--- src/main/java/com/putterfly/simpk/drawer/KSDrawer.java
+++ src/main/java/com/putterfly/simpk/drawer/KSDrawer.java
@@ -15,10 +15,10 @@ import java.util.Map;
 public class KSDrawer {
     public static void draw(Canvas canvas, List<String> labels,List<Pair<String,String>> relations){
         final int w = 90;
-        final int h = 60;
+        final int h = 90;
         final int spanH = 40;
         final int spanV = 40;
-        Map<String,int[]> nodes = new HashMap<>();
+        Map<String,double[]> nodes = new HashMap<>();
         GraphicsContext graphicsContext2D = canvas.getGraphicsContext2D();
         graphicsContext2D.clearRect(0,0,canvas.getWidth(),canvas.getHeight());
         graphicsContext2D.fillText("Kripke Structure Graph Transferred from the input program:",spanH,(double)spanV/2);
@@ -31,28 +31,30 @@ public class KSDrawer {
             for(int j = 0;j<positionX;j++){
                 String label = iter.next();
                 if(!iter.hasNext()) break;
-                nodes.put(label,new int[]{j*(w+spanH),positionY});
+                nodes.put(label,new double[]{j*(w+spanH)+(double)w/2,positionY+(double)w/2});
             }
         }
 
         for(Pair<String,String> r : relations){
             if(r.getValue()==null||r.getValue().isEmpty()) continue;
             if(r.getValue().equals(r.getKey())){
-                int[] first = nodes.get(r.getValue());
+                double[] first = nodes.get(r.getValue());
                 if (first==null) continue;
-                graphicsContext2D.strokeLine((double)(first[0]+(double)w/2),first[1],(double)(first[0]+w+5),first[1]);
-                graphicsContext2D.strokeLine((double)(first[0]+5+w),first[1],(double)(first[0]+5+w),(double)(first[1]+(double)h/2) );
-                double[] getArr = calVertexes((double)(first[0]+5+w),(double)(first[1]+(double)h/2),(double)(first[0]+w),(double)(first[1]+(double)h/2));
-                graphicsContext2D.strokeLine((double)(first[0]+5+w),(double)(first[1]+(double)h/2),(double)(first[0]+w),(double)(first[1]+(double)h/2));
-                graphicsContext2D.strokeLine((double)(first[0]+w),(double)(first[1]+(double)h/2),getArr[0],getArr[1]);
-                graphicsContext2D.strokeLine((double)(first[0]+w),(double)(first[1]+(double)h/2),getArr[2],getArr[3]);
+                graphicsContext2D.strokeLine(first[0],first[1],first[0]+w/2+6,first[1]);
+                graphicsContext2D.strokeLine(first[0]+w/2+6,first[1],first[0]+w/2+6,first[1]+(double)h/2 );
+                double[] des = calDes(first[0]+w/2+6,first[1]+(double)h/2,first[0],first[1],w);
+                double[] getArr = calVertexes((double)(first[0]+w/2+6),(double)(first[1]+(double)h/2),des[0],des[1]);
+                graphicsContext2D.strokeLine((double)(first[0]+w/2+6),(double)(first[1]+(double)h/2),des[0],des[1]);
+                graphicsContext2D.strokeLine(des[0],des[1],getArr[0],getArr[1]);
+                graphicsContext2D.strokeLine(des[0],des[1],getArr[2],getArr[3]);
                 continue;
             }
-            int[] start = nodes.get(r.getKey());
-            int[] end = nodes.get(r.getValue());
+            double[] start = nodes.get(r.getKey());
+            double[] end = nodes.get(r.getValue());
             if(end==null||start ==null) continue;
-            double[] scenter = new double[]{start[0]+(double)w/2,start[1]};
-            double[] ecenter = new double[]{end[0]+(double)h/2,(double)(end[1]+10)};
+            double[] scenter = new double[]{start[0],start[1]};
+            double[] ecenter = new double[]{end[0],end[1]};
+            ecenter = calDes(scenter[0],scenter[1],ecenter[0],ecenter[1],w);
             double[] arr = calVertexes(scenter[0],scenter[1],ecenter[0],ecenter[1]);
             graphicsContext2D.strokeLine(scenter[0],scenter[1],ecenter[0],ecenter[1]);
             graphicsContext2D.strokeLine(ecenter[0],ecenter[1],arr[0],arr[1]);
@@ -61,6 +63,7 @@ public class KSDrawer {
         positionX = 0;
         positionY = 0;
         iter = labels.iterator();
+        int count = 0;
         while(iter.hasNext()){
             positionX++;
             positionY += h+spanV;
@@ -72,12 +75,18 @@ public class KSDrawer {
                 graphicsContext2D.setTextAlign(TextAlignment.CENTER);
                 graphicsContext2D.setTextBaseline(VPos.CENTER);
                 graphicsContext2D.setFill(Color.WHITE);
-                graphicsContext2D.fillText(label,Math.round(j*(w+spanH)+(double)w/2),Math.round(positionY+(double)h/2));
+                graphicsContext2D.fillText("S"+count,Math.round(j*(w+spanH)+(double)w/2),Math.round(positionY+(double)h/2));
+                count++;
             }
         }
 
     }
 
+    private static double[] calDes(double sx,double sy,double ex,double ey,int r){
+        double angle = Math.atan2(ey-sy,ex-sx);
+        return new double[]{ex-Math.cos(angle)*r/2,ey-Math.sin(angle)*r/2};
+
+    }
     private static double[] calVertexes(double sx,double sy,double ex,double ey){
         double length = 6;
         double degree = 0.5;
